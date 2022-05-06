279f7da777629b34ba685715a58affe949d69edd
Phillip2608 <luizlindo2416@gmail.com>
2022-05-06 17:55:02 -0300

    Add cases from future pages on project

--- index.js
+++ index.js
@@ -2,11 +2,28 @@ const http = require('http');
 
 let server = http.createServer((req, res) => {
 
-    console.log('URL:'+req.url);
-    console.log('METHOS'+ req.method);
+    console.log('URL: '+req.url);
+    console.log('METHOD: '+ req.method);
 
-    res.end('OK');
+    switch(req.url){
+        case '/':
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "text/html");
+            res.end("<h1>Hello</h1>");
+        break;
 
+        case '/users':
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "application/json");
+            res.end(JSON.stringify({
+                users:[{
+                    name: 'Calanguinho',
+                    email: 'calanguinho@calangin.com',
+                    id: 1
+                }]
+            }));
+        break;
+    }
 });
 
 server.listen(3000, '127.0.0.1', () =>{
