bd56cc152701073487f826a2dfdc5fda76637faf
Phillip2608 <luizlindo2416@gmail.com>
2022-05-06 17:39:51 -0300

    Add server on project

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("fala otario!");
+const http = require('http');
+
+let server = http.createServer((req, res) => {
+
+    console.log('URL:'+req.url);
+    console.log('METHOS'+ req.method);
+
+    res.end('OK');
+
+});
+
+server.listen(3000, '127.0.0.1', () =>{
+
+    console.log("Servidor rodando!");
+
+});
