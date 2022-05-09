18bee15306b476f8b12614de3b97eb952f32637d
Phillip2608 <luizlindo2416@gmail.com>
2022-05-09 18:02:02 -0300

    Add the express-validation in the post and put

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const consign = require('consign');
 const bodyParser = require('body-parser');
+const {body, validationResult} = require('express-validator');
 
 let app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+app.use(body());
 
 consign().include('routes').include('utils').into(app);
 
--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,4 @@
+const { validationResult, body } = require('express-validator');
 let NeDB = require('nedb');
 let db = new NeDB({
     filename: 'users.db',
@@ -18,17 +19,28 @@ module.exports = (app) => {
         });
     });
     
-    route.post((req, res) => {
+    route.post(
 
-        db.insert(req.body, (err, user) =>{
-            if(err){
-                app.utils.error.send(err, req, res);
+        body('name', 'O nome e obrigatorio').isLength({min:1}),
+        body('email', 'O email e invalido').isEmail(),
+        body('passw', 'A senha deve possuir no minimo 8 digitos').isLength({min: 8}),
+        ((req, res) => {
+            const errors  = validationResult(req);
+
+            if(!errors.isEmpty()){
+                return res.status(400).json({errors: errors.array()});
             }else{
-                res.status(200).json(user);
+                db.insert(req.body, (err, user) =>{
+                    if(err){
+                        app.utils.error.send(err, req, res);
+                    }else{
+                        res.status(200).json(user);
+                    }
+                });
             }
-        });
+        })
 
-    });
+    );
                                     //email
                                     //name
     let routeId = app.route('/users/:id');
@@ -45,15 +57,27 @@ module.exports = (app) => {
         });
     });
 
-    routeId.put((req, res) => {
-        db.update({_id:req.params.id}, req.body, err=>{
-            if(err){
-                app.utils.error.send(err, req, res);
+    routeId.put(
+        body('name', 'O nome e obrigatorio').isLength({min:1}),
+        body('email', 'O email e invalido').isEmail(),
+        body('passw', 'A senha deve possuir no minimo 8 digitos').isLength({min: 8}),
+        ((req, res) => {
+            const errors  = validationResult(req);
+
+            if(!errors.isEmpty()){
+                return res.status(400).json({errors: errors.array()});
             }else{
-                res.status(200).json(Object.assign(req.body, req.params));
+                db.update({_id:req.params.id}, req.body, err=>{
+                    if(err){
+                        app.utils.error.send(err, req, res);
+                    }else{
+                        res.status(200).json(Object.assign(req.body, req.params));
+                    }
+                });
             }
-        });
-    });
+        }),
+        
+    );
 
     routeId.delete((req, res) => {
 
