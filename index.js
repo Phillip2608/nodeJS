101eb73796de88efc013dcdff64080a84cd0014e
Phillip2608 <luizlindo2416@gmail.com>
2022-05-09 13:51:03 -0300

    now the server initializes with express

--- index.js
+++ index.js
@@ -1,32 +1,27 @@
-const http = require('http');
+const express = require('express');
 
-let server = http.createServer((req, res) => {
+let app = express();
 
-    console.log('URL: '+req.url);
-    console.log('METHOD: '+ req.method);
-
-    switch(req.url){
-        case '/':
-            res.statusCode = 200;
-            res.setHeader("Content-Type", "text/html");
-            res.end("<h1>HelloAA</h1>");
-        break;
-
-        case '/users':
-            res.statusCode = 200;
-            res.setHeader("Content-Type", "application/json");
-            res.end(JSON.stringify({
-                users:[{
-                    name: 'Calanguinho',
-                    email: 'calanguinho@calangin.com',
-                    id: 1
-                }]
-            }));
-        break;
-    }
+app.get('/',(req, res) => {
+    
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    res.end("<h1>HelloAA</h1>");
 });
 
-server.listen(3000, '127.0.0.1', () =>{
+app.get('/users',(req, res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.json({
+        users:[{
+            name: 'Calanguinho',
+            email: 'calanguinho@calangin.com',
+            id: 1
+        }]
+    });
+});
+
+app.listen(3000, '127.0.0.1', () =>{
 
     console.log("Servidor rodando!");
 
