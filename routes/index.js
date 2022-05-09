77438144a1ce471f0aa6b043856b95d2c471c73d
Phillip2608 <luizlindo2416@gmail.com>
2022-05-09 14:30:02 -0300

    now the system use a dependency consign decreasing code

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,7 @@
-let express = require('express');
-const { route } = require('express/lib/application');
-
-let routes = express.Router();
-
-routes.get('/',(req, res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/html");
-    res.end("<h1>HelloAA</h1>");
-});
-
-module.exports = routes;
+module.exports = app =>{
+    app.get('/',(req, res) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/html");
+        res.end("<h1>HelloAA</h1>");
+    });
+};
